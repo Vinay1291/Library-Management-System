2e02cd7708dcc2614a0b9c466e9898f67bf72b43
Vinay Vishwakarma <vinayv1291@gmail.com>
2025-04-16 00:14:04 +0530

    was trying to add dark mode toggle.

--- text/home/script.js
+++ text/home/script.js
@@ -16,3 +16,19 @@ hamburger.addEventListener('click', () => {
   navMenu.classList.toggle('active');
   hamburger.classList.toggle('active'); // for animation
 });
+
+// Hamburger
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+  hamburger.classList.toggle('active');
+});
+
+// Dark Mode
+const toggle = document.getElementById('darkModeToggle');
+const body = document.body;
+
+toggle.addEventListener('change', () => {
+  body.classList.toggle('dark-mode');
+});
