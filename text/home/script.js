e0eb7fc875c8374f93fb6533de93ec8581155ba6
Vinay Vishwakarma <vinayv1291@gmail.com>
2025-04-16 16:50:56 +0530

    sign in and sign up form up Done

--- text/home/script.js
+++ text/home/script.js
@@ -1,11 +1,22 @@
-const container = document.getElementById('container');
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
+const signInButton = document.querySelector('#signInButton');
+const signUpButton = document.querySelector('#signUpButton');
+const overlay_container = document.querySelector('.container .overlay-container');
+const overlay = document.querySelector('.container .overlay-container .overlay');
+const signInForm = document.querySelector('.container .sign-in-form');
+const signUpForm = document.querySelector('.container .sign-up-form');
 
-signUpButton.addEventListener('click', () => {
-  container.classList.add("right-panel-active");
+
+signInButton.addEventListener('click', ()=>{
+    overlay_container.style.transform = 'translateX(100%)';
+    overlay.style.transform= 'translateX(-50%)';
+    signInForm.classList.add('active');
+    signUpForm.classList.remove('active');
 });
 
-signInButton.addEventListener('click', () => {
-  container.classList.remove("right-panel-active");
+signUpButton.addEventListener('click', ()=>{
+    overlay_container.style.transform = 'translateX(0)';
+    overlay.style.transform= 'translateX(0)';
+    signUpForm.classList.add('active');
+    signInForm.classList.remove('active');
 });
+
