e75b77b9e45062642380760e6c46eed7e29e874c
Vinay Vishwakarma <vinayv1291@gmail.com>
2025-04-29 20:04:38 +0530

    admin side is done.

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -3,7 +3,7 @@
       <li class="<?= ($activePage === 'dashboard') ? 'active' : '' ?>"><a href="../admin/">Dashboard</a></li>
       <li class="<?= ($activePage === 'resources') ? 'active' : '' ?>"><a href="resources.php">Resources</a></li>
       <li class="<?= ($activePage === 'manage-books') ? 'active' : '' ?>"><a href="manage-books.php">Manage Books</a></li>
-      <li class="<?= ($activePage === 'reports') ? 'active' : '' ?>"><a href="reports.php">Reports</a></li>
+      <li class="<?= ($activePage === 'lend') ? 'active' : '' ?>"><a href="lend_book.php">Lend Book</a></li>
       <li class="<?= ($activePage === 'lended-books') ? 'active' : '' ?>"><a href="lended.php">Lended Books</a></li>
       <li class="<?= ($activePage === 'members') ? 'active' : '' ?>"><a href="members.php">Members</a></li>
       <li class="<?= ($activePage === 'settings') ? 'active' : '' ?>"><a href="settings.php">Settings</a></li>
